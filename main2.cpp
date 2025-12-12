46e93475f95a79d54b2265cbf27fe4aa682279f2
Achira1234 <achira.por@student.mahidol.edu>
2025-12-12 19:21:58 +0700

    Implement parentheses checking in command line arguments
    Added parentheses matching functionality to main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -1,29 +1,58 @@
-#include<iostream>
+#include <iostream>
+#include <cstring>
 using namespace std;
 
 #include "stack.h"
 
-int main(int argc, char **argv){
-    Stack s;
-   
-/*
-  Exercise 2
- printf("Checking the parentheses in argv arguments\n");
-
-   */
-
-    /*
-     for(j=0;j<strlen(argv[i]);j++){
-       // Use stack to help with the parentheses
-
-
-
-
-
-  }
-
-  */
-
-
-   return 0;
+bool match(int open, char close) {
+    return (open == '(' && close == ')') ||
+           (open == '[' && close == ']') ||
+           (open == '{' && close == '}');
 }
+
+int main(int argc, char **argv) {
+
+    cout << "Checking the parentheses in argv arguments\n";
+
+    for (int i = 1; i < argc; i++) {
+        cout << "\nArgument: " << argv[i] << endl;
+
+        Stack s;
+        bool ok = true;
+
+        for (int j = 0; j < strlen(argv[i]); j++) {
+            char c = argv[i][j];
+
+
+            if (c == '(' || c == '[' || c == '{') {
+                s.push((int)c);
+            }
+
+
+            else if (c == ')' || c == ']' || c == '}') {
+                int open = s.pop();
+
+                if (open == -1) {
+                    ok = false;
+                    break;
+                }
+
+                if (!match(open, c)) {   // wrong type
+                    ok = false;
+                    break;
+                }
+            }
+        }
+
+        int leftover = s.pop();
+        if (leftover != -1) ok = false;
+
+        if (ok)
+            cout << "→ Parentheses OK\n";
+        else
+            cout << "→ Parentheses NOT OK\n";
+    }
+
+    return 0;
+}
+
